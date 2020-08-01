ed0d277eadbee781c826b11a3e0940aec37d208a
Mohammad Reza Dehghani Tafti <dehghani735@gmail.com>
2020-08-01 15:59:45 +0430

    990511 sample

--- src/main/java/com/mdt/tacobar/OrderRepository.java
+++ src/main/java/com/mdt/tacobar/OrderRepository.java
@@ -2,7 +2,12 @@ package com.mdt.tacobar;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
 //    Order save(Order order);
+
+    //customizing JPA repositories; it will select from Order because we introduce it in CrudRepository
+    List<Order> findByDeliveryZip(String deliveryZip);
 }
