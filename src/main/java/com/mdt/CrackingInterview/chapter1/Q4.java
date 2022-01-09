78259046e3d2ccfed4572b0b9f4ef0718e529eca
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-01-09 17:19:22 +0330

    Q4 added.

--- src/main/java/com/mdt/CrackingInterview/chapter1/Q4.java
+++ src/main/java/com/mdt/CrackingInterview/chapter1/Q4.java
@@ -17,10 +17,118 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class Q4 {
 
-    private static final Logger log = getLogger(Q3.class);
+    private static final Logger log = getLogger(Q4.class);
 
+    /**
+     * This is a question where it helps to figure out what it means for a string to be a permutation of a palindrome.
+     * This is like asking what the "defining features" of such a string would be.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        var q4 = new Q4();
+        // Note
+        log.info(String.valueOf(Character.getNumericValue('a')));
+        log.info(String.valueOf(Character.getNumericValue('A')));
 
+        log.info(String.valueOf(q4.isPermutationOfPalindrome1("Tact Coa")));
+        log.info(String.valueOf(q4.isPermutationOfPalindrome2("Tact Coa")));
+        log.info(String.valueOf(q4.isPermutationOfPalindrome3("Tact Coa")));
     }
 
+    boolean isPermutationOfPalindrome1(String str) {
+        var table = buildCharFrequencyTable(str);
+        return checkOneOdd(table);
+    }
+
+    /**
+     * @param str
+     * @return
+     */
+    private int[] buildCharFrequencyTable(String str) {
+        var table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for (var c : str.toCharArray()) {
+            var num = getCharNumber(c);
+            if (num != -1)
+                table[num]++;
+        }
+
+        return table;
+    }
+
+    private boolean checkOneOdd(int[] table) {
+        var alreadyFoundOdd = false;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] % 2 != 0) {
+                if (alreadyFoundOdd)
+                    return false;
+                alreadyFoundOdd = true;
+            }
+        }
+
+        return true;
+    }
+
+    private int getCharNumber(char c) {
+        var a   = Character.getNumericValue('a'); // Important Note: a and A has the same numeric value!
+        var z   = Character.getNumericValue('z');
+        var val = Character.getNumericValue(c);
+
+        if (val >= a && val <= z)
+            return val - a;
+
+        return -1;
+    }
+
+    boolean isPermutationOfPalindrome2(String str) {
+        var table        = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        var numberOfOdds = 0;
+        for (var c : str.toCharArray()) {
+            var num = getCharNumber(c);
+            if (num != -1) {
+                table[num]++;
+                if (table[num] % 2 != 0)
+                    numberOfOdds++;
+                else
+                    numberOfOdds--;
+            }
+        }
+
+        return numberOfOdds <= 1;
+    }
+
+    /**
+     * idea: bit vector
+     */
+    boolean isPermutationOfPalindrome3(String str) {
+        var bitVector = createBitVector(str);
+        return bitVector == 0 || atMostOneBitSet(bitVector);
+    }
+
+    private boolean atMostOneBitSet(int bitVector) {
+        return (bitVector & (bitVector - 1)) == 0;
+    }
+
+    private int createBitVector(String str) {
+        var bitVector = 0;
+        for (var c : str.toCharArray()) {
+            bitVector = toggle(bitVector, c);
+        }
+
+        return bitVector;
+    }
+
+    private int toggle(int bitVector, char c) {
+        var num = getCharNumber(c);
+        if (num < 0)
+            return bitVector;
+
+        var mask = 1 << num;
+        if ((bitVector & mask) == 0)
+            bitVector |= mask;
+        else
+            bitVector &= ~mask;
+
+        return bitVector;
+    }
 }
