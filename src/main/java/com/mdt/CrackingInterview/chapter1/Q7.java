adcb1e2a07285203cccc38fde06f6cb936648ddf
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-01-13 16:41:03 +0330

    Q1.8 added.

--- src/main/java/com/mdt/CrackingInterview/chapter1/Q7.java
+++ src/main/java/com/mdt/CrackingInterview/chapter1/Q7.java
@@ -1,5 +1,9 @@
 package com.mdt.CrackingInterview.chapter1;
 
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * 1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
  * bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
@@ -7,12 +11,15 @@ package com.mdt.CrackingInterview.chapter1;
  * date: 1/11/22
  */
 public class Q7 {
+    private static final Logger log = getLogger(Q7.class);
+
     public static void main(String[] args) {
-        var q7 = new Q7();
+        var q7     = new Q7();
         var result = rotateMatrix();
     }
 
     // TODO implement
-    private static void rotateMatrix() {
+    private static String rotateMatrix() {
+        return "";
     }
 }
---
+++ src/main/java/com/mdt/CrackingInterview/chapter1/Q9.java
@@ -0,0 +1,13 @@
+package com.mdt.CrackingInterview.chapter1;
+
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class Q9 {
+    private static final Logger log = getLogger(Q9.class);
+
+    public static void main(String[] args) {
+
+    }
+}
