f5ec50274fb568d18475ccbf64104365a501cc4b
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-01-15 23:41:24 +0330

    Q2.1 added.

---
+++ src/main/java/com/mdt/CrackingInterview/chapter2/Q2.java
@@ -0,0 +1,12 @@
+package com.mdt.CrackingInterview.chapter2;
+
+/**
+ * 2.2 Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+ * <p>
+ * date: 1/15/22
+ */
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+}
