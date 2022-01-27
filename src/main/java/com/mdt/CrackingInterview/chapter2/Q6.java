3e07146bca757c7197d7bfccb07fe5e55596b66f
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-01-27 14:57:08 +0330

    Q2.6 revised.

--- src/main/java/com/mdt/CrackingInterview/chapter2/Q6.java
+++ src/main/java/com/mdt/CrackingInterview/chapter2/Q6.java
@@ -2,6 +2,8 @@ package com.mdt.CrackingInterview.chapter2;
 
 import org.slf4j.Logger;
 
+import java.util.Stack;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -17,6 +19,7 @@ public class Q6 {
         var n1 = new LinkedListNode(0);
         var n2 = new LinkedListNode(5);
         var n3 = new LinkedListNode(4);
+        var nm = new LinkedListNode(10);
         var n4 = new LinkedListNode(4);
         var n5 = new LinkedListNode(5);
         var n6 = new LinkedListNode(0);
@@ -25,16 +28,23 @@ public class Q6 {
         n2.setPrevious(n1);
         n2.setNext(n3);
         n3.setPrevious(n2);
-        n3.setNext(n4);
-        n4.setPrevious(n3);
+        n3.setNext(nm);
+        nm.setPrevious(n3);
+        nm.setNext(n4);
+        n4.setPrevious(nm);
         n4.setNext(n5);
         n5.setPrevious(n4);
         n5.setNext(n6);
         n6.setPrevious(n5);
         n6.setNext(null);
 
-        var result = q6.checkPalindrome1(n1);
-        log.info(String.valueOf(result));
+        var input1  = n1;
+        var result1 = q6.checkPalindrome1(input1);
+        log.info(String.valueOf(result1));
+
+        var input2  = n1;
+        var result2 = q6.checkPalindrome2(input2);
+        log.info(String.valueOf(result2));
     }
 
     /**
@@ -84,6 +94,31 @@ public class Q6 {
      * @return
      */
     public boolean checkPalindrome2(LinkedListNode l1) {
+        var stack  = new Stack<>();
+        var runner = l1;
+        while (runner != null && runner.next != null) {
+            stack.push(l1.data);
+            l1     = l1.next;
+            runner = runner.next.next;
+        }
+        if (runner != null)
+            l1 = l1.next;
+        while (l1 != null) {
+            if (l1.data != (int) stack.pop())
+                return false;
+            l1 = l1.next;
+        }
+        return true;
+    }
 
+    /**
+     * idea: Recursive
+     *
+     * @param l1
+     * @return
+     */
+    public boolean checkPalindrome3(LinkedListNode l1) {
+//        TODO implement later
+        return false;
     }
 }
