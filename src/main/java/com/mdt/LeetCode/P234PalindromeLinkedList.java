71e91bc9a3aaf0c9b9f8b105763929c40e53bfda
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-06-09 11:19:22 +0430

    Palindrome linked list added.

---
+++ src/main/java/com/mdt/LeetCode/P234PalindromeLinkedList.java
@@ -0,0 +1,58 @@
+package com.mdt.LeetCode;
+
+/**
+ * Easy
+ * Given the head of a singly linked list, return true if it is a palindrome.
+ * date: 6/9/22
+ * <p>
+ * TODO: recursive solution is availble in cracking book?
+ */
+public class P234PalindromeLinkedList {
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+            var slow = head;
+            var fast = head;
+
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            slow = reversed(slow);
+            fast = head;
+
+            while (slow != null) {
+                if (slow.val != fast.val)
+                    return false;
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return true;
+        }
+
+        ListNode reversed(ListNode head) {
+            ListNode prev = null;
+            while (head != null) {
+                var next = head.next;
+                head.next = prev;
+                prev      = head;
+                head      = next;
+            }
+            return prev;
+        }
+
+        class ListNode {
+            int      val;
+            ListNode next;
+
+            ListNode()        {}
+
+            ListNode(int val) {this.val = val;}
+
+            ListNode(int val, ListNode next) {
+                this.val  = val;
+                this.next = next;
+            }
+        }
+    }
+}
