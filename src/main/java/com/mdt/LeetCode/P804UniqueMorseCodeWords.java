9c747dd0b7b74f04a2dc82dd21116823a69be6d2
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-06-15 23:46:19 +0430

    unique morse codes.

---
+++ src/main/java/com/mdt/LeetCode/P804UniqueMorseCodeWords.java
@@ -0,0 +1,23 @@
+package com.mdt.LeetCode;
+
+import java.util.HashSet;
+
+public class P804UniqueMorseCodeWords {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        var set = new HashSet<String>();
+        var dic = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        for (var s : words) {
+            var sb = new StringBuilder();
+            for (var c : s.toCharArray())
+                sb.append(dic[c - 'a']);
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
