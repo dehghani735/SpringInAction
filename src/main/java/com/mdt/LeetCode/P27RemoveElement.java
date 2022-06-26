e2c08294beb15971df9082c741f97d1ac19920c0
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-06-27 01:04:10 +0430

    remove element

---
+++ src/main/java/com/mdt/LeetCode/P27RemoveElement.java
@@ -0,0 +1,36 @@
+package com.mdt.LeetCode;
+
+/**
+ * Easy
+ * <p>
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+ * <p>
+ * Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+ * <p>
+ * Return k after placing the final result in the first k slots of nums.
+ * <p>
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * date: 6/26/22
+ */
+public class P27RemoveElement {
+
+    /**
+     * nick's solution
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        var validSize = 0;
+
+        for (var i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[validSize] = nums[i];
+                validSize++;
+            }
+        }
+        return validSize;
+    }
+}
