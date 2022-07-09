e0ac0704ad291ec2466fcc92673abd3b973a3b3f
Mohammad Reza Dehghani <m.dehghani@jibit.ir>
2022-07-09 10:35:18 +0430

    repeated substring pattern added.

---
+++ src/main/java/com/mdt/LeetCode/P459RepeatedSubstringPattern.java
@@ -0,0 +1,31 @@
+package com.mdt.LeetCode;
+
+/**
+ * Easy
+ * <p>
+ * Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+ * <p>
+ * date: 7/09/22
+ */
+public class P459RepeatedSubstringPattern {
+
+    /**
+     * Nick's solution, interesting
+     *
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        var len = s.length();
+        for (var i = len / 2; i > 0; i--) {
+            if (len % i == 0) {
+                var numRepeats = len / i;
+                var substring  = s.substring(0, i);
+
+                if (substring.repeat(numRepeats).equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
