4d6f9ca4a7bef1d31f60372ab1dbbdd0f30ebb65
mr.tafti <dehghani735@gmail.com>
2023-04-12 22:31:50 +0330

    Fluent interfaces added.

---
+++ src/main/java/com/mdt/FunctionalAndReactive/M9_4_Decorator/Burger.java
@@ -0,0 +1,34 @@
+package com.mdt.FunctionalAndReactive.M9_4_Decorator;
+
+public class Burger {
+
+    private String burgerType;
+
+    // set burger with no decoration
+    public Burger() {
+        this.burgerType = "";
+    }
+
+    // only to be used inside this class
+    private Burger(String type) {
+        this.burgerType = type;
+    }
+
+    // modify the functionality or to add decorations
+    public Burger addVeggies() {
+        System.out.println("Adding veggies to the burger");
+
+        return new Burger(this.burgerType += " Veggie"); // returns the fresh instance of the burger class. we could modified the same instance,
+        // but in FP we don't modify the object's state
+    }
+
+    public Burger addCheese() {
+        System.out.println("Adding cheese to the burger");
+
+        return new Burger(this.burgerType += " Cheese");
+    }
+
+    public String toString() {
+        return String.format("%s", burgerType + " burger");
+    }
+}
