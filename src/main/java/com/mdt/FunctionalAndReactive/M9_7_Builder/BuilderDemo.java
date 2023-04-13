54814c6aa45123943a50e5ef858f6e2fd16fc40f
mr.tafti <dehghani735@gmail.com>
2023-04-13 10:19:27 +0330

    Builder pattern added.

---
+++ src/main/java/com/mdt/FunctionalAndReactive/M9_7_Builder/BuilderDemo.java
@@ -0,0 +1,16 @@
+package com.mdt.FunctionalAndReactive.M9_7_Builder;
+
+public class BuilderDemo {
+
+    public static void main(String[] args) {
+
+        MobileBuilder builder = new MobileBuilder();
+        Mobile myMobile = builder.with(myBuilder -> {
+            myBuilder.ram = 4;
+            myBuilder.battery = 4000;
+            myBuilder.processor = "A12 Bionic";
+        }).createMobile();  // no need to define multiple constructors. we can define default fields.
+
+        System.out.println(myMobile);
+    }
+}
---
+++ src/main/java/com/mdt/FunctionalAndReactive/M9_7_Builder/MobileBuilder.java
@@ -0,0 +1,21 @@
+package com.mdt.FunctionalAndReactive.M9_7_Builder;
+
+import java.util.function.Consumer;
+
+public class MobileBuilder {
+
+    int ram, storage;
+    int battery;
+    int camera;
+    String processor;
+    double screenSize;
+
+    public MobileBuilder with(Consumer<MobileBuilder> buildFields) {
+        buildFields.accept(this);
+        return this;
+    }
+
+    public Mobile createMobile() {
+        return new Mobile(this);
+    }
+}
