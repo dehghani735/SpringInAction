e38985fceebf03b33d42568307eb13a18533f4b2
mr.tafti <dehghani735@gmail.com>
2023-04-13 18:14:20 +0330

    Calculation statistics added.

---
+++ src/main/java/com/mdt/FunctionalAndReactive/M10_8_Reduce/ReduceOperation.java
@@ -0,0 +1,12 @@
+package com.mdt.FunctionalAndReactive.M10_8_Reduce;
+
+import java.util.stream.Stream;
+
+public class ReduceOperation {
+
+    public static void main(String[] args) {
+        var x = Stream.of(1, 2, 4, 67, 87, 65, 46)
+                .reduce(0, (a, b) -> a + b);
+        System.out.println(x);
+    }
+}
